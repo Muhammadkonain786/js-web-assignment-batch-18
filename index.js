368f818c0d8f8d42fc704f28098d6e72925949cd
Muhammadkonain786 <konainshahid70@gmail.com>
2026-01-23 09:23:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function handleButtonClick() {
+    const myButton = document.getElementById("myButton");
+    const div2img = document.getElementById("div2img");
+    myButton.style.display = "none";
+    div2img.style.display = "block";
+}
+
+function buttonclick() {
+    alert("Add to card!");
+}
